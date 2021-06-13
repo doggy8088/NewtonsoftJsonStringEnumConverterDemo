c4255cfb2be8c5a976054cc4bc16fd1988762f86
Will <doggy.huang@gmail.com>
2021-06-13 18:43:31 +0800

    使用 System.Net.Http.Json 的 HttpClientJsonExtensions 擴充方法來取得 API 回傳的強型別內容

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Net.Http;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using QuickType;
+using System.Threading.Tasks;
 
 namespace c1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             using var client = new HttpClient();
+
+            var result = await client.GetFromJsonAsync<Holiday>("https://data.taipei/api/v1/dataset/29d9771d-c0ee-40d4-8dfb-3866b0b7adaa?scope=resourceAquire&offset=958&limit=1000");
         }
     }
 }
